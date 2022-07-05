9dd02be20b2286f20606f975c409011e63c38fe9
Randhir200 <mail.randhirkr@gmail.com>
2022-07-05 23:07:41 +0530

    routing learning

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+function App() {
+  return (
+    <div className='App'>
+      <Navbar />
+      <Routes>
+        <Route path={'/'} element={<Home />}></Route>
+        <Route path={'/about'} element={<About />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
